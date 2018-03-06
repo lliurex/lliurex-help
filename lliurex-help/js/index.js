7ad5484af89192ac81dace9707b3d38de6a20917
Jose A. Múrcia <joamuran@gmail.com>
2018-03-06 15:16:55 +0100

    wip on multilanguage help

--- lliurex-help/js/index.js
+++ lliurex-help/js/index.js
@@ -55,16 +55,38 @@ function main(){
 
     var index=fs.readFileSync(indexpath);
     var indexContent=JSON.parse(index);
+    // Getting locales from index file
+    var languages=(Object.keys(indexContent));
+    
+    // Setting up locale selector ui
+    for (i=0; i<languages.length; i++){
+        var newlang=document.createElement("div");
+        newlang.setAttribute("class", "langItem");
+        console.log(languages[i]);
+        newlang.innerHTML="["+languages[i]+"]";
+        newlang.setAttribute("localeCode", languages[i]);
+        document.querySelector("#locales").appendChild(newlang);
+    }
 
-    // Getting locales
+    // Getting current locale
 
-    var default_locale=indexContent[lang];    
-    if (typeof (default_locale) === typeof (undefined)) default_locale=indexContent[keys(intexContent)[0]];
+    lang="es";
+    var default_locale=indexContent[lang];
+    console.log(default_locale);
+    console.log(indexContent);
+
+    if (typeof (default_locale) === typeof (undefined)){
+        var first_index=Object.keys(indexContent)[0];
+        default_locale=indexContent[first_index];
+    }
+
+    
+
 
     // Getting menu
 
     var menu=createMenu(default_locale, "mainMenu");
-    (document.querySelector("#sections")).appendChild(menu);
+    (document.querySelector("#menu")).appendChild(menu);
 
 
     // Manage events
@@ -83,6 +105,43 @@ function main(){
             //document.querySelector("#content").remove();
             document.querySelector("#content").innerHTML=html;
         }
+        else if (e.target.matches("li")){
+            // Matches with an li item, without mdDoc class.
+
+            e.stopPropagation();
+            //console.log(e.target.parentNode);
+             console.log(e.target);
+             var className = " link ";
+             var haslink=( (" " + e.target.className + " ").replace(/[\n\t]/g, " ").indexOf(" link ") > -1 );
+             console.log(haslink);
+             if (haslink){
+
+                 // Setting children visibility
+                 console.log("***"+e.target.children[0].visiblechild);
+                 console.log(e.target.children[0]);
+                 if (e.target.children[0].visiblechild==="none")
+                      e.target.children[0].visiblechild="block";
+                  else e.target.children[0].visiblechild="none"
+
+                 for (i=0; i<e.target.children[0].children.length; i++){
+                  console.log(i);
+                     console.log(e.target.children[0].children[i]);
+                     
+                     e.target.children[0].children[i].style.display=e.target.children[0].visiblechild;
+                 }
+             }
+        } else if (e.target.matches(".langItem")){
+            //alert(e.target.getAttribute("localeCode"));
+            var newContent=indexContent[e.target.getAttribute("localeCode")];
+            var menu=createMenu(newContent, "mainMenu");
+            (document.querySelector("#menu")).innerHTML="";
+            (document.querySelector("#menu")).appendChild(menu);
+
+            // WIP: Queda posar fora l'associació d'events (altra func)
+            //      i aci buidar la pàgina de continguts i fer el clic al primer item.
+            
+        }
+
 
 
 
@@ -93,10 +152,11 @@ function main(){
 
 
       // TO-DO: Posar açò dalt...
-
+/*
       var listitems=document.querySelectorAll("li");
       for (li=0; li<listitems.length;li++){
           listitems[li].addEventListener("click",function(e) {
+
               e.stopPropagation();
               //console.log(e.target.parentNode);
                console.log(e.target);
@@ -124,7 +184,7 @@ function main(){
           });
           //li.addeventlistener("");
       }
-
+*/
 
 
 
